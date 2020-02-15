85709d99031ccde209e072ee98db5283b89ba1f3
jjohnsundar <jjohnsundar9943@gmail.com>
2020-02-15 13:37:10 +0530

    Updates

---
+++ precache-manifest.900b150a51e9e6aa4b8f58fc59607e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7c277751f8bf71f10a2e8f64895abc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "b7da1095cf04c7cf0e23",
+    "url": "/calculator/static/css/main.9590549a.chunk.css"
+  },
+  {
+    "revision": "a7db7b5a17cb22e1be92",
+    "url": "/calculator/static/js/2.8835306b.chunk.js"
+  },
+  {
+    "revision": "b7da1095cf04c7cf0e23",
+    "url": "/calculator/static/js/main.565f4e84.chunk.js"
+  },
+  {
+    "revision": "4e3319d03c4ebf7772c1",
+    "url": "/calculator/static/js/runtime-main.49e08181.js"
+  }
+]);
